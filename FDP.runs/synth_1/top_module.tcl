e03196d25fa0876ae961d3c1d9c364d1cb3b873a
dorndorn54 <samuellee20523@gmail.com>
2025-10-01 15:46:12 +0800

    implementation of top module, just need to call in the other task

--- FDP.runs/synth_1/top_module.tcl
+++ FDP.runs/synth_1/top_module.tcl
@@ -23,22 +23,22 @@ create_project -in_memory -part xc7a35tcpg236-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.cache/wt [current_project]
-set_property parent.project_path C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.cache/wt [current_project]
+set_property parent.project_path C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.cache/ip [current_project]
+set_property ip_output_repo c:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib {
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/colour_identifier.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/debouncer.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/new/freq_200.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/freq_clock_gen.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/oled_display.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/pixel_output_generator.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/new/seven_segment_display.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/task_q.v
-  C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/sources_1/new/top_module.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/colour_identifier.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/debouncer.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/new/freq_200.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/freq_clock_gen.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/oled_display.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/pixel_output_generator.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/new/seven_segment_display.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/imports/samue/lab_4_task_q/lab_4_task_q.srcs/sources_1/new/task_q.v
+  C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/sources_1/new/top_module.v
 }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -48,8 +48,8 @@ read_verilog -library xil_defaultlib {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/constrs_1/imports/new/my_basys3_constraints.xdc
-set_property used_in_implementation false [get_files C:/Users/samue/Documents/vivado/ee2026_b02_g10/FDP.srcs/constrs_1/imports/new/my_basys3_constraints.xdc]
+read_xdc C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/constrs_1/imports/new/my_basys3_constraints.xdc
+set_property used_in_implementation false [get_files C:/Users/dorn/Documents/xilinx/ee_2026_project/ee2026_b02_g10/FDP.srcs/constrs_1/imports/new/my_basys3_constraints.xdc]
 
 set_param ips.enableIPCacheLiteLoad 0
 close [open __synthesis_is_running__ w]
--- FDP.srcs/sources_1/new/top_module.v
+++ FDP.srcs/sources_1/new/top_module.v
@@ -28,6 +28,32 @@ module top_module(
     output [7:0] seg,
     output [7:0] JB
 );
+    //connections for frequency clock
+    wire freq625m;
+    //connectiojns for the task_q
+    wire [15:0] pixel_data_q;
+    //connections for the multiplexer, i am just gonna place the multiplexer in the top
+    wire [15:0] pixel_data_multiplexed;
+    //connections for the oled_display
+    wire [12:0] pixel_index;
+    wire frame_begin, sending_pixels, sample_pixel;
+    
+    //frequency clock function
+    freq_6_25m f0(clk, freq625m); //input is the native clock, output is 6.25Mhz signal to run the oled display
+    
     // Always running seven segment
-    seven_segment_display f0(clk, an, seg);
+    seven_segment_display f1(clk, an, seg); //clk is the native clokc input
+    
+    //initiate the separate task here
+    task_q t0(clk, btnL, btnC, btnR, pixel_index, pixel_data_q);
+    //clk is the native clock, btn are direct, pixel_index is the output of the display
+    //pixel_data_q is the output of this module fed back to the oled_display function later
+    
+    assign pixel_data_multiplexed = sw[15] ? pixel_data_q : 16'b11111_111111_11111;
+    
+        oled_display f2(freq625m, 0, frame_begin, sending_pixels, sample_pixel, 
+                   pixel_index, pixel_data_multiplexed, JB[0], JB[1], JB[3], JB[4], JB[5], JB[6], JB[7]);
+                   //freq625m comes from the clock above the next 4 on the same row not important
+                   //pixel_index goes to the tasks and pixel_data goes into this module from the task
+                   //that is the multiplexed part
 endmodule
