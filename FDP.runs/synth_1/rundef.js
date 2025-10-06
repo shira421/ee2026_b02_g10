84e7ac549064921dd1cbd5af44cf70e74e218530
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-10-06 09:18:42 +0800

    Update Top_Student.v

--- FDP.runs/synth_1/rundef.js
+++ FDP.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log Top_Student.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source Top_Student.tcl" );
+         "-log task_r.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source task_r.tcl" );
 
 
 
--- FDP.srcs/sources_1/new/Top_Student.v
+++ FDP.srcs/sources_1/new/Top_Student.v
@@ -1,10 +1,11 @@
 `timescale 1ns / 1ps
 
-module Top_Student (
+module task_r(
     input clk,
     input btnC,
     input [4:0] sw,
-    output [7:0] JB  
+    input [12:0] pixel_index,
+    output reg [15:0] oled_data
 );
     reg clk6p25m = 0;
     reg [3:0] clk_div = 0;
@@ -17,15 +18,7 @@ module Top_Student (
             clk_div <= clk_div + 1;
         end
     end
-
-    wire frame_begin, sending_pixels, sample_pixel;
-    wire [12:0] pixel_index;
-    reg [15:0] oled_data;
-
-    assign JB[2] = 1'b0;
-    assign JB[6] = 1'b1;
-    assign JB[7] = 1'b1;
-
+    
     reg [6:0] x_pos_blue = 10;
     reg [6:0] y_pos_orange = 10;
     reg dir_blue = 1;           
@@ -84,21 +77,4 @@ module Top_Student (
         if (draw1) oled_data = 16'h001F;
         if (draw9) oled_data = 16'hFD20;
     end
-
-    Oled_Display oled_inst (
-        .clk(clk6p25m),
-        .reset(btnC),
-        .frame_begin(frame_begin),
-        .sending_pixels(sending_pixels),
-        .sample_pixel(sample_pixel),
-        .pixel_index(pixel_index),
-        .pixel_data(oled_data),
-        .cs(JB[0]),
-        .sdin(JB[1]),
-        .sclk(JB[3]),
-        .d_cn(JB[4]),
-        .resn(JB[5]),
-        .vccen(),
-        .pmoden()
-    );
 endmodule
