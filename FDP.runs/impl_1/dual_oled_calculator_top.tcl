d2d6cf09a69f30035ffa7b61710220471732e472
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-10-11 02:08:53 +0800

    Update file structure

--- FDP.runs/impl_1/dual_oled_calculator_top.tcl
+++ FDP.runs/impl_1/dual_oled_calculator_top.tcl
@@ -61,23 +61,92 @@ proc step_failed { step } {
 }
 
 
-start_step write_bitstream
-set ACTIVE_STEP write_bitstream
+start_step init_design
+set ACTIVE_STEP init_design
 set rc [catch {
-  create_msg_db write_bitstream.pb
-  open_checkpoint dual_oled_calculator_top_routed.dcp
+  create_msg_db init_design.pb
+  set_param xicom.use_bs_reader 1
+  create_project -in_memory -part xc7a35tcpg236-1
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
   set_property webtalk.parent_dir C:/Users/vkshe/OneDrive/Documents/NUS/ee2026/ee2026_b02_g10/FDP.cache/wt [current_project]
-  catch { write_mem_info -force dual_oled_calculator_top.mmi }
-  write_bitstream -force dual_oled_calculator_top.bit 
-  catch {write_debug_probes -quiet -force dual_oled_calculator_top}
-  catch {file copy -force dual_oled_calculator_top.ltx debug_nets.ltx}
-  close_msg_db -file write_bitstream.pb
+  set_property parent.project_path C:/Users/vkshe/OneDrive/Documents/NUS/ee2026/ee2026_b02_g10/FDP.xpr [current_project]
+  set_property ip_output_repo C:/Users/vkshe/OneDrive/Documents/NUS/ee2026/ee2026_b02_g10/FDP.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet C:/Users/vkshe/OneDrive/Documents/NUS/ee2026/ee2026_b02_g10/FDP.runs/synth_1/dual_oled_calculator_top.dcp
+  read_xdc C:/Users/vkshe/OneDrive/Documents/NUS/ee2026/ee2026_b02_g10/FDP.srcs/constrs_1/imports/new/my_basys3_constraints.xdc
+  link_design -top dual_oled_calculator_top -part xc7a35tcpg236-1
+  close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
-  step_failed write_bitstream
+  step_failed init_design
   return -code error $RESULT
 } else {
-  end_step write_bitstream
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force dual_oled_calculator_top_opt.dcp
+  create_report "impl_1_opt_report_drc_0" "report_drc -file dual_oled_calculator_top_drc_opted.rpt -pb dual_oled_calculator_top_drc_opted.pb -rpx dual_oled_calculator_top_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design 
+  write_checkpoint -force dual_oled_calculator_top_placed.dcp
+  create_report "impl_1_place_report_io_0" "report_io -file dual_oled_calculator_top_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file dual_oled_calculator_top_utilization_placed.rpt -pb dual_oled_calculator_top_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file dual_oled_calculator_top_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force dual_oled_calculator_top_routed.dcp
+  create_report "impl_1_route_report_drc_0" "report_drc -file dual_oled_calculator_top_drc_routed.rpt -pb dual_oled_calculator_top_drc_routed.pb -rpx dual_oled_calculator_top_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file dual_oled_calculator_top_methodology_drc_routed.rpt -pb dual_oled_calculator_top_methodology_drc_routed.pb -rpx dual_oled_calculator_top_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file dual_oled_calculator_top_power_routed.rpt -pb dual_oled_calculator_top_power_summary_routed.pb -rpx dual_oled_calculator_top_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file dual_oled_calculator_top_route_status.rpt -pb dual_oled_calculator_top_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file dual_oled_calculator_top_timing_summary_routed.rpt -pb dual_oled_calculator_top_timing_summary_routed.pb -rpx dual_oled_calculator_top_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file dual_oled_calculator_top_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file dual_oled_calculator_top_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file dual_oled_calculator_top_bus_skew_routed.rpt -pb dual_oled_calculator_top_bus_skew_routed.pb -rpx dual_oled_calculator_top_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force dual_oled_calculator_top_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
   unset ACTIVE_STEP 
 }
 
--- FDP.runs/impl_1/rundef.js
+++ FDP.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log dual_oled_calculator_top.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source dual_oled_calculator_top.tcl -notrace" );
 
